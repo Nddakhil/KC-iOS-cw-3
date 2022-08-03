36d9531a260fdef381769f73b33a93d8603a478e
Nddakhil <109868614+Nddakhil@users.noreply.github.com>
2022-08-03 19:51:56 +0300

    cw3

---
+++ Day 3/MyPlayground.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var favouriteNumber:Int = 3
+var name:String = "Noor"
+var hobby = "electric drumming"
+
+var birthofdate = "2005"
+
+print("Hello My name is \(name)and my favourite Number is \(favouriteNumber) and my hobby is \(hobby) and I was born in \(birthofdate)")
